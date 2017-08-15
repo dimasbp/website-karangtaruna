9b5870d1dfea780561857ae68427763f4dc69ca0
Axelyo Primastomo <axelyoprimastomo@gmail.com>
2017-08-15 11:11:41 +0700

    zerif

---
+++ Source Code Theme/content.php
@@ -0,0 +1,141 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemtype="http://schema.org/BlogPosting" itemtype="http://schema.org/BlogPosting">
+
+	<?php if ( ! is_search() ) : ?>
+
+
+
+		<?php
+        $post_thumbnail_url = get_the_post_thumbnail( get_the_ID(), 'zerif-post-thumbnail' );
+
+		if ( ! empty( $post_thumbnail_url ) ): ?>
+
+            <div class="post-img-wrap">
+
+                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+
+                    <?php echo $post_thumbnail_url; ?>
+
+                </a>
+
+            </div>
+
+		<div class="listpost-content-wrap">
+
+		<?php else: ?>
+
+		<div class="listpost-content-wrap-full">
+
+		<?php endif; ?>
+
+	<?php else:  ?>
+
+			<div class="listpost-content-wrap-full">
+
+	<?php endif; ?>
+
+	<div class="list-post-top">
+
+	<header class="entry-header">
+
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+
+		<div class="entry-meta">
+
+			<?php zerif_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
+
+		<?php endif; ?>
+
+	</header><!-- .entry-header -->
+
+	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+
+	<div class="entry-summary">
+
+		<?php the_excerpt(); ?>
+		
+	<?php else : ?>
+
+	<div class="entry-content">
+
+		<?php
+
+		    $ismore = ! empty( $post->post_content ) ? strpos( $post->post_content, '<!--more-->') : '';
+
+		    if ( !empty($ismore) ) {
+				the_content( sprintf( esc_html__('[&hellip;]','zerif-lite'), '<span class="screen-reader-text">'.esc_html__('about ', 'zerif-lite').get_the_title().'</span>' ) );
+			} else {
+				the_excerpt();
+			}
+			
+			wp_link_pages( array(
+
+				'before' => '<div class="page-links">' . __( 'Pages:', 'zerif-lite' ),
+
+				'after'  => '</div>',
+
+			) );
+
+		endif; ?>
+
+	<footer class="entry-footer">
+
+		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+
+			<?php
+
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ', ', 'zerif-lite' ) );
+
+				if ( $categories_list && zerif_categorized_blog() ) :
+
+			?>
+
+			<span class="cat-links">
+
+				<?php printf( __( 'Posted in %1$s', 'zerif-lite' ), $categories_list ); ?>
+
+			</span>
+
+			<?php endif; // End if categories ?>
+
+			<?php
+
+				/* translators: used between list items, there is a space after the comma */
+
+				$tags_list = get_the_tag_list( '', __( ', ', 'zerif-lite' ) );
+
+				if ( $tags_list ) :
+
+			?>
+
+			<span class="tags-links">
+
+				<?php printf( __( 'Tagged %1$s', 'zerif-lite' ), $tags_list ); ?>
+
+			</span>
+
+			<?php endif; // End if $tags_list ?>
+
+		<?php endif; // End if 'post' == get_post_type() ?>
+
+		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'zerif-lite' ), __( '1 Comment', 'zerif-lite' ), __( '% Comments', 'zerif-lite' ) ); ?></span>
+
+		<?php endif; ?>
+
+		<?php edit_post_link( __( 'Edit', 'zerif-lite' ), '<span class="edit-link">', '</span>' ); ?>
+
+	</footer><!-- .entry-footer -->
+
+	</div><!-- .entry-content --><!-- .entry-summary -->
+
+	</div><!-- .list-post-top -->
+
+</div><!-- .listpost-content-wrap -->
+
+</article><!-- #post-## -->
